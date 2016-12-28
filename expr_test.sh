3006e6ce32d4e5cb5bdc46ee8e9b56b75fa5dabf
eric54205420 <eric9400@gmail.com>
2016-12-28 11:06:56 +0800

    Create expr_test.sh

---
+++ expr_test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+num1=4
+num2=3
+
+echo "num1 = $num1, num2 = $num2"
+echo ""
+echo -n "num1 + num2 = "
+expr $num1 + $num2
+echo -n "num1 - num2 = "
+expr $num1 - $num2
+echo -n "num1 * num2 = " # must use "\" to handle * operator
+expr $num1 \* $num2
+echo -n "num1 / num2 = "
+expr $num1 / $num2
+echo -n "num1 % num2 = "
+expr $num1 % $num2
+
+
+exit 0
