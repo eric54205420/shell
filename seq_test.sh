2f711959f689f91788f3083e13507c6f0775c070
eric54205420 <eric9400@gmail.com>
2016-12-28 17:56:14 +0800

    Update seq_test.sh

--- seq_test.sh
+++ seq_test.sh
@@ -5,11 +5,34 @@
 seq 1 10
 
 
-# odds begin with 1 less than 10
-for integer in `seq 1 2 10`
-do
-    echo $integer
-done
+# generate 1 to 10 step 2 ( odds less than 10 )
+seq 1 2 10
+
+# print out from a to z
+echo {a..z}
+
+
+# print out seqence with different IFS " "
+# default IFs=$'\n'
+seq -s " " 1 10
+
+# print out with equal width
+seq -s " " -w 1 20
+
+# print out with special form
+seq -s " " -f "The %05gth" 1 50
+
+# 1a ~ 10z and 11 ~ 99
+echo {1..10}{a..z} 
+echo {1..9}{1..9} 
+
+# work with for loop
+#for integer in `seq 1 2 10`
+#do
+#    echo $integer
+#done
+#
+
 
 
 exit 0
